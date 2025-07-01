eaa9115de8f54c56566c760377d7d4750b619fa9
Damini Raj <daminiraj70@gmail.com>
2025-07-01 09:56:29 +0530

    customer responsive

--- Pyop app/src/pages/Customer/login/Login.jsx
+++ Pyop app/src/pages/Customer/login/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
   };
 
   return (
-    <div className="px-4 mt-20">
+    <div className="px-4 mt-[7rem]">
       {/* <h2>Login</h2> */}
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
